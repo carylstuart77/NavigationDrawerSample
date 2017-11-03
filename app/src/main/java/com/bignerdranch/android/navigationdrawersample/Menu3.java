cd034ddb4c4245ac0a6c66d17879a421dc682119
Caryl Stuart <carylstuart@email.com>
2017-11-03 10:26:51 -0600

    everything

---
+++ app/src/main/java/com/bignerdranch/android/navigationdrawersample/Menu3.java
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.navigationdrawersample;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Menu3 extends Fragment {
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+   return inflater.inflate(R.layout.menu3, container, false);
+  }
+
+  @Override
+  public void onViewCreated(View view, Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    getActivity().setTitle("Menu 3");
+  }
+}
